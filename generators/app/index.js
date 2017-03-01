f380c0f134d8be280f91d88a8d1254d3536a3618
Benjamin Reid <benjamin@nouveller.com>
2017-03-01 10:57:15 +0000

    Warn about sub generators

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,11 +1,10 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-  constructor(args, opts) {
-    super(args, opts)
-  }
-
-  hello() {
-    this.log('Hello from the React Native generator');
+  end() {
+    this.log('Oops! There is no generator here.');
+    this.log('If you’re creating a new app from scratch please run:');
+    this.log('react-native init <ProjectName> && cd <ProjectName>');
+    this.log('You can then run any of the sub generators inside your project.');
   }
 };
