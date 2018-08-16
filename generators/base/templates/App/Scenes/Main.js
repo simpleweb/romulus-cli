f29ccf40fda6b3bddf4de5e7efe0a5f62279b18a
Benjamin Reid <ben@simpleweb.co.uk>
2018-08-16 15:39:36 +0100

    Add i18n support

--- generators/base/templates/App/Scenes/Main.js
+++ generators/base/templates/App/Scenes/Main.js
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { Text } from 'react-native';
 import { type NavigationState } from 'react-navigation';
+import { t } from "<%= name %>/App/Helpers/Translations";
 import Layout from '<%= name %>/App/Components/Layout';
 import Button from '<%= name %>/App/Components/Button';
 
@@ -12,9 +13,9 @@ type Props = {
 function Main({ navigation }: Props): React.Node {
   return (
     <Layout.Center>
-      <Text>Main</Text>
+      <Text>{t("scenes.main.title")}</Text>
       <Button onPress={() => navigation.navigate("Styleguide")}>
-        Styleguide
+        {t("scenes.main.styleguide_button")}
       </Button>
     </Layout.Center>
   );
