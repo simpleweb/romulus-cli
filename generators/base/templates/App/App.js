e63156b25a3d8116f3fd7bf6a0fc026a74c2d519
Benjamin Reid <ben@simpleweb.co.uk>
2018-08-17 16:51:06 +0100

    Clean up store

--- generators/base/templates/App/App.js
+++ generators/base/templates/App/App.js
@@ -1,36 +1,32 @@
 // @flow
-import * as React from 'react';
-import { Platform } from 'react-native';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react'
-import RootNavigator from '<%= name %>/App/Router';
-import { configureStore } from '<%= name %>/App/Store';
-import { runSagaMiddleware } from '<%= name %>/App/Store/Middleware/Saga';
-import App from '<%= name %>/App/Components/App';
+import * as React from "react";
+import { Platform } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import Router from "<%= name %>/App/Router";
+import { store, persistor } from "<%= name %>/App/Store";
+import { runSagaMiddleware } from "<%= name %>/App/Store/Middleware/Saga";
+import App from "<%= name %>/App/Components/App";
 
-const { persistor, store } = configureStore();
+const prefix =
+  Platform.OS === "android" ? "<%= name %>://<%= name %>/" : "<%= name %>://";
 
-const prefix = Platform.OS === 'android' ? '<%= name %>://<%= name %>/' : '<%= name %>://';
-
-export default class <%= name %> extends React.Component<{}> {
-
-  _onBeforeLift = () => {
-    runSagaMiddleware();
-  }
-
+class <%= name %> extends React.Component<{}> {
   render(): React.Node {
     return (
       <App>
         <Provider store={store}>
           <PersistGate
             loading={null}
-            onBeforeLift={this._onBeforeLift}
+            onBeforeLift={runSagaMiddleware}
             persistor={persistor}
           >
-            <RootNavigator uriPrefix={prefix} />
+            <Router uriPrefix={prefix} />
           </PersistGate>
         </Provider>
       </App>
     );
   }
 }
+
+export default <%= name %>;
--- generators/base/templates/App/Store/index.js
+++ generators/base/templates/App/Store/index.js
@@ -1,18 +1,8 @@
 // @flow
-import { createStore, compose } from 'redux';
-import { persistStore } from 'redux-persist';
-import Reducers from '<%= name %>/App/Reducers';
-import Middleware from '<%= name %>/App/Store/Middleware';
+import { createStore, compose } from "redux";
+import { persistStore } from "redux-persist";
+import Reducers from "<%= name %>/App/Reducers";
+import Middleware from "<%= name %>/App/Store/Middleware";
 
-const store = createStore(
-  Reducers,
-  compose(Middleware)
-);
-
-const configureStore = () => {
-  const persistor = persistStore(store);
-  return { persistor, store };
-}
-
-export default store;
-export { configureStore };
+export const store = createStore(Reducers, compose(Middleware));
+export const persistor = persistStore(store);
