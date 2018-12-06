b48a97a4fd01dae9870bc85bd6f0e60867c2c593
Benjamin Reid <ben@simpleweb.co.uk>
2018-12-06 17:12:06 +0000

    Space the text out and add emoji wave

--- generators/base/templates/App/Components/Text/index.js
+++ generators/base/templates/App/Components/Text/index.js
@@ -4,6 +4,7 @@ import { Text as StyledText } from "./styles";
 
 type Props = {
   children: React.Node,
+  paragraph?: boolean,
 };
 
 const Text = (props: Props): React.Node => <StyledText {...props} />;
