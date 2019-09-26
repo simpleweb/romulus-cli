5054c36c52e64eabd0dfc2d5dea59c7c5edbf402
Benjamin Reid <ben@simpleweb.co.uk>
2019-09-26 16:35:28 +0100

    Convert app to FC

--- generators/base/templates/App/index.js
+++ generators/base/templates/App/index.js
@@ -9,24 +9,22 @@ import { store, persistor } from "<%= name %>/App/Store";
 import { runSagaMiddleware } from "<%= name %>/App/Store/Middleware/Saga";
 import App from "<%= name %>/App/Components/App";
 
-class <%= name %> extends React.Component<{}> {
-  render(): React.Node {
-    return (
-      <ThemeProvider theme={Theme}>
-        <App>
-          <Provider store={store}>
-            <PersistGate
-              loading={null}
-              onBeforeLift={runSagaMiddleware}
-              persistor={persistor}
-            >
-              <Router uriPrefix="<%= name %>://" />
-            </PersistGate>
-          </Provider>
-        </App>
-      </ThemeProvider>
-    );
-  }
+function <%= name %>() {
+  return (
+    <ThemeProvider theme={Theme}>
+      <App>
+        <Provider store={store}>
+          <PersistGate
+            loading={null}
+            onBeforeLift={runSagaMiddleware}
+            persistor={persistor}
+          >
+            <Router uriPrefix="<%= name %>://" />
+          </PersistGate>
+        </Provider>
+      </App>
+    </ThemeProvider>
+  );
 }
 
 export default <%= name %>;
