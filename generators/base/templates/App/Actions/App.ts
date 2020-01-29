9656f05aa4575ce37e1ae0b9246f1df8cf5e0eab
Benjamin Reid <ben@simpleweb.co.uk>
2020-01-29 15:47:00 +0000

    Fix reducers and actions

--- generators/base/templates/App/Actions/App.ts
+++ generators/base/templates/App/Actions/App.ts
@@ -1,9 +1,11 @@
-export const APP_INSTALLED = '<%= name %>/APP_INSTALLED';
+export const APP_INSTALLED = "<%= name %>/APP_INSTALLED";
 
-export type AppInstalled = {
-  type: '<%= name %>/APP_INSTALLED',
-};
+interface AppInstalled {
+  type: typeof APP_INSTALLED;
+}
 
 export const appInstalled = (): AppInstalled => ({
   type: APP_INSTALLED,
 });
+
+export type AppActions = AppInstalled;
