dbc7f11af6796a680ec4e8a5c4b47e9609ee40c8
Benjamin Reid <ben@simpleweb.co.uk>
2020-01-29 15:02:13 +0000

    Fix Button component

--- generators/base/templates/App/Components/Button/index.tsx
+++ generators/base/templates/App/Components/Button/index.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import { Container, ButtonText } from "./styles";
 
-type Props = {
-  onPress: Function,
-  children: React.Node,
-};
+interface Props {
+  onPress: () => any;
+}
 
-const Button = ({ onPress, children }: Props): React.Node => (
+const Button: React.FC<Props> = ({ onPress, children }) => (
   <TouchableOpacity onPress={onPress}>
     <Container>
       <ButtonText>{children}</ButtonText>
