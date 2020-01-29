37844b5e5323774b6638edd223ee20e7f797477d
Benjamin Reid <ben@simpleweb.co.uk>
2020-01-29 14:59:54 +0000

    Fix App component

--- generators/base/templates/App/Components/App/index.tsx
+++ generators/base/templates/App/Components/App/index.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { ENV } from "<%= name %>/App/Config";
 import Environment from "<%= name %>/App/Components/Utilities/Environment";
 
-type Props = {
-  children: React.Node,
-};
-
-const App = ({ children }: Props): React.Node => (
+const App: React.FC = ({ children }) => (
   <React.Fragment>
     {children}
     <Environment env={ENV} />
