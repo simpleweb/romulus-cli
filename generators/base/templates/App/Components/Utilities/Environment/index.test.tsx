c92dbc3289545fa7e1531a9152de736c048c2fb6
Benjamin Reid <ben@simpleweb.co.uk>
2020-02-11 13:00:29 +0000

    Add more tests for the other environments

--- generators/base/templates/App/Components/Utilities/Environment/index.test.tsx
+++ generators/base/templates/App/Components/Utilities/Environment/index.test.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import { render } from "test-utilities";
 import Environment from "./index";
 
-it("renders a badge specific to the environment", () => {
+it("renders a development badge", () => {
   const { getByText } = render(<Environment env="development" />);
   expect(getByText("D")).toBeDefined();
+});
+
+it("renders a edge badge", () => {
+  const { getByText } = render(<Environment env="edge" />);
+  expect(getByText("E")).toBeDefined();
+});
+
+it("renders a staging", () => {
+  const { getByText } = render(<Environment env="staging" />);
+  expect(getByText("S")).toBeDefined();
 });
 
 it("doesn’t render in production", () => {
