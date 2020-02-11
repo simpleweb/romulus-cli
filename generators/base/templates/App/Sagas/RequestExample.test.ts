f16078b070b5ddb75cb79fd1ee34cfdf976e9e36
Benjamin Reid <ben@simpleweb.co.uk>
2020-02-11 15:11:10 +0000

    Add cases for errors

--- generators/base/templates/App/Sagas/RequestExample.test.ts
+++ generators/base/templates/App/Sagas/RequestExample.test.ts
@@ -8,13 +8,41 @@ beforeEach(() => {
   jest.resetAllMocks();
 });
 
-it("should get a color", async () => {
-  get.mockReturnValueOnce({ data: "hello" });
+it("should respond with the example data", async () => {
+  get.mockReturnValue({ data: "hello" });
 
   const dispatched = await recordSaga(RequestExample);
 
   expect(dispatched).toContainEqual({
     type: "REQUEST_EXAMPLE_SUCCESS",
     result: { data: "hello" },
+  });
+});
+
+it("should handle a server error", async () => {
+  get.mockRejectedValue({
+    response: { message: "Couldn’t find that user" },
+  });
+
+  const dispatched = await recordSaga(RequestExample);
+
+  expect(dispatched).toContainEqual({
+    type: "REQUEST_EXAMPLE_ERROR",
+    error: {
+      response: {
+        message: "Couldn’t find that user",
+      },
+    },
+  });
+});
+
+it("should handle other errors", async () => {
+  get.mockRejectedValue(new Error());
+
+  const dispatched = await recordSaga(RequestExample);
+
+  expect(dispatched).toContainEqual({
+    type: "REQUEST_EXAMPLE_ERROR",
+    error: "Network error",
   });
 });
