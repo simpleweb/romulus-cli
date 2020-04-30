aae9fe1b982572155aaf14cb7a7082d8be151952
andykitt <andypkitt@gmail.com>
2020-04-30 17:51:57 +0100

    fastlane script

---
+++ generators/base/templates/bin/fastlane.sh
@@ -0,0 +1,31 @@
+# current path
+current=$PWD
+
+#fastlane init
+fastlaneInit(){
+  echo "Adding Fastlane to iOS..."
+  cd $current && cd ios && fastlane init
+  echo "Adding Fastlane to Android..."
+  cd .. && cd android && fastlane init
+  cd ..
+  cd bin && rm fastlane.sh
+}
+
+# check if fastlane is installed
+isInstalled() {
+if hash fastlane 2>/dev/null; then
+    fastlaneInit
+else
+    echo "How do you want to install Fastlane?"
+select yn in "RubyGems" "Homebrew"; do
+    case $yn in
+        RubyGems ) echo "Installing Fastlane via RubyGems (you maybe asked for a password)... "; sudo gem install fastlane -NV ; break;;
+        Homebrew ) echo "Installing Fastlane via Homebrew..."; brew install fastlane ; break;;
+    esac
+    done
+    fastlaneInit
+fi
+};
+
+echo "Installing fastlane..."
+isInstalled;
